3f47f5b62b46d273394068cabf0fad1a636d5a6d
EC2 Default User <ec2-user@ip-172-31-41-88.us-west-2.compute.internal>
2018-04-04 17:29:45 +0000

    Changed IP in spinup to new server

--- TorRouter/spinup.sh
+++ TorRouter/spinup.sh
@@ -4,6 +4,6 @@
 for i in `seq 0 ${1}`;
 do
 #    echo ARGS="35.160.105.33 800${i}" PORT=808${i} CONT=${i}
-    make build ARGS="35.160.105.33 8000" PORT=808${i}
-    make juststart ARGS="35.160.105.33 800${i}" PORT=808${i} CONT=${i}
+    make build ARGS="34.216.100.202 8000" PORT=808${i}
+    make juststart ARGS="34.216.100.202 800${i}" PORT=808${i} CONT=${i}
 done
