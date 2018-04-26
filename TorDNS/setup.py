e650b6249270ecc9ca40bdab630ba9208677704c
Ben Janis <benjamin.janis@tufts.edu>
2018-04-26 17:02:37 -0400

    Added empty TorDNS package

---
+++ TorDNS/setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TorDNS',
+      version='1.0',
+      description='Tor DNS server',
+      author='Allen, Ben, Toby',
+      packages=['TorDNS'],
+     )
--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ python2.7 -m pip install --upgrade pycryptodome
 python2.7 -m pip install --upgrade Crypt/
 python2.7 -m pip install --upgrade TorPathingServer/
 python2.7 -m pip install --upgrade TorRouter/
+python2.7 -m pip install --upgrade TorDNS/
